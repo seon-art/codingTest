142a6fe935fcb8934c699f63308220227db49d66
seon-art <104439452+seon-art@users.noreply.github.com>
2025-04-22 21:44:20 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.19 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = Integer.MIN_VALUE;;
+        
+        for(int i = 0; i < numbers.length; i++) {
+            if(i == numbers.length - 1) {
+                break;
+            }
+            
+            for(int j = i + 1 ; j < numbers.length; j++) {
+                if(numbers[i] * numbers[j] > answer) {
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return answer;
+    }
+}
